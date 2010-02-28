089fd7cc58ba364e4d7dc77020f4fa0b6f0335dc
mizchi <miz404@gmail.com>
2010-03-01 00:37:25 +0900

    fix

--- Resources/py/tipy.py
+++ Resources/py/tipy.py
@@ -1,34 +1,29 @@
 #/usr/bin/python
 #encoding:utf-8
-import sys,os
-sys.path.append(os.pardir)
 
 import twixy
 from time import sleep
 
-
 tw=twixy.Api(
     localStorage.id,
-    localStorage.password)
-_id_last_tl=0
-_id_last_tl_replies=0
+    localStorage.password,
+    proxy="http://twixy53.appspot.com/api"
+    )
+
+
+# jQuery(document.body).ready(jQuery("#tabs").tabs())
+
 
 def PostTwitter(text):
     tw.PostUpdate(test) 
 
+last_id=0
 def update():
-    global tw
-    tw=twixy.Api(
-        localStorage.id,
-        localStorage.password,
-        proxy="http://twixy53.appspot.com/api"
-        )
-    
-    tl = tw.GetHomeTimeline(count=150,since_id=_id_last_tl)
+    tl = tw.GetHomeTimeline(count=150,since_id=last_id)
     twbody = document.getElementById("twitter")
 
     node=document.createElement("li")
-    node.innerHTML=u'<div style="border-bottom:dotted 1px black;height:1px;">.</div>'
+    node.innerHTML=u'<div style="background-color:black;height:1px;">.</div>'
     twbody.insertBefore(node,twbody.firstChild)
         
     for i in tl[::-1]:
@@ -40,37 +35,31 @@ def update():
         if i.text.find(localStorage.id) >0:
             updateReplies()
             
-    global _id_last_tl
-    _id_last_tl=tl[0].id
+    global last_id
+    last_id=tl[0].id
     
+last_id_replies=0
 def updateReplies():
-    tl = tw.GetReplies(since_id=_id_last_tl_replies)
+    tl = tw.GetReplies(since_id=last_id_replies)
     rep    = document.getElementById("replies")
     for i in tl[::-1]:
         node=document.createElement("li")
         node.setAttribute("class","tw_container")
         node.innerHTML=u'<div class="icon_box"><img class="icon"src="%s"/></div><span class="username">%s</span><span class="text">%s</span>' %(i.user.profile_image_url,i.user.screen_name,i.text)
         rep.insertBefore(node,rep.firstChild)
-    global _id_last_tl_replies
-    _id_last_tl_replies=tl[0].id
+    global last_id_replies
+    last_id_replies=tl[0].id
     return False
+    
 
 def startup():
     window.resizeTo(1280,800)
     window.moveTo(0,0)
     update()
-    sleep(1)
+    sleep(1);
     updateReplies()
     # setInterval(update,1000*15)
     
-
-# key binding
-k_state=[]
-for i in range(200):
-    k_state.append(0)
-ev_cur=1
-input_flag=1
-
 kcode={
     "h":72,
     "j":74,
@@ -92,8 +81,11 @@ kcode={
     "3":51    
     }
 
-def doEvent():
-    global ev_cur
+k_state=[]
+for i in range(200):
+    k_state.append(0)
+ev_cur=1
+def onEvent():
     if k_state[kcode["j"]]:
         scrollBy(0,50)
     if k_state[kcode["k"]]:
@@ -109,28 +101,43 @@ def doEvent():
     if k_state[kcode["3"]]:
         ev_cur=3
         jQuery("#t3").click()
+    # if k_state[kcode[H]] and (ev_cur > 1))
+    # 	jQuery("#t"+(--ev_cur)).click();
+    # if(k_state[kcode[L] && (ev_cur < 3))
+    # 	jQuery("#t"+(++ev_cur)).click();
     if k_state[kcode["w"]]:
-        if jQuery("span.text").css("white-space") =="nowrap" :
-            jQuery(".icon").css("margin","0")
-            jQuery(".icon_box").css("height","40px") 
-            jQuery("span.text").css("white-space","normal")
-        elif jQuery("span.text").css("white-space") =="normal" :
-            jQuery(".icon").css("margin","-18px 0px;")
-            jQuery(".icon_box").css("height","18px")
-            jQuery("span.text").css("white-space","nowrap")
+	jQuery("span.text").css("white-space","nowrap")
+    if k_state[kcode["w"]] and k_state[kcode[SHIFT]]:
+	jQuery("span.text").css("white-space","normal")
 
-    if k_state[kcode["g"]] :
-        window.scrollTo(0,0)
-    if k_state[kcode["q"]] and k_state[kcode["ctrl"]]:
-	Titanium.App.exit()
-    # return False 
 
+    # if(k_state_before[K_STATE_G] && k_state[K_STATE_G])window.scrollTo(0,0);
     
-def changeKeys(keyCode,to):
+    # if(k_state[K_STATE_G] && k_state[K_STATE_SHIFT])
+    #     window.scrollTo(0,10000);
+    # if(k_state[K_STATE_R] && (ev_cur==1))
+    #     update();
+    # if(k_state[K_STATE_R] && (ev_cur==2))updateReplies();
+    if k_state[kcode["q"]] and k_state[kcode["ctrl"]]:
+	window.close();
+
+    return False 
+
+def changeKeys(keycode,to):
     global k_state
-    jQuery("#key_check").text(keyCode)
-    k_state[int(keyCode)]=to
-    doEvent()
-    if ev_cur ==3 :
-	return True
-    return False;
+    k_state[int(keycode)]=to
+    onEvent()
+
+
+# jwindow=jQuery(window).keydown
+# def jQuery(window).keydown(e):
+#     changeKeys(e.keyCode,1)
+#     jQuery("#key_check").text(e.keyCode)
+#     return False
+
+# def jQuery(window).keyup(e):
+#     changeKeys(e.keyCode,0)
+#     jQuery("#key_check").text(e.keyCode)
+#     return False
+
+
